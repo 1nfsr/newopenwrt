4e2e36c7a162e8b9f160747d3fbc57ac519cecaa
Infsr <i@infsr.me>
2021-01-21 18:55:06 +0800

    update

--- SCRIPTS/06_cleaning.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf `/bin/ls | grep -v -E '(combined|manifest)'`
-gzip -d *.gz
-gzip --best --keep *.img
-sha256sum openwrt* | tee sha256_$(date "+%Y%m%d").hash
-md5sum    openwrt* | tee    md5_$(date "+%Y%m%d").hash
-rm -f *.img
-exit 0
