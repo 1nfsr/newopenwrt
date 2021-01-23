a1be23bcf61aa065aae5af90071757cbbb9164bf
Infsr <i@infsr.me>
2021-01-23 18:08:40 +0800

    add app

--- script-p2.sh
+++ script-p2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
+
+mkdir -p package/diy
+git clone https://github.com/frainzy1477/luci-app-clash.git package/diy/luci-app-clash
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/diy/luci-app-adguardhome
