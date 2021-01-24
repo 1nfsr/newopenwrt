3e31e3da1e2cc6531a20f1df656497d9d04aa24f
Infsr <i@infsr.me>
2021-01-24 17:38:37 +0800

    update

--- script-p2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
-
-mkdir -p package/diy
-git clone https://github.com/frainzy1477/luci-app-clash.git package/diy/luci-app-clash
-git clone https://github.com/kongfl888/luci-app-adguardhome.git package/diy/luci-app-adguardhome
