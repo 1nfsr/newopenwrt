ec97897743f70e5d9ab35b8eba8b57655a149913
Infsr <i@infsr.me>
2021-01-25 22:57:09 +0800

    add adg&openclash

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,3 +2,11 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+mkdir -p package/apps
+
+#adg
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/apps/luci-app-adguardhome
+
+#openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/apps/luci-app-openclash
