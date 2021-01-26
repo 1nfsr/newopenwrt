280828c8b2fae0144340f050485c59ba6c328762
Infsr <i@infsr.me>
2021-01-27 06:34:32 +0800

    fullcontnat sfe

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,45 @@ git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/ap
 
 #openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/apps/luci-app-openclash
+
+
+# patch dnsmasq
+mv ../patch/* ./
+wget -P package/network/services/dnsmasq/patches/ https://github.com/QiuSimons/R2S-R4S-X86-OpenWrt/raw/master/PATCH/new/package/900-add-filter-aaaa-option.patch
+patch -p1 < ./dnsmasq-add-filter-aaaa-option.patch
+patch -p1 < ./luci-add-filter-aaaa-option.patch
+
+## fullcone
+# Patch Kernel 解决fullcone冲突
+pushd target/linux/generic/hack-5.4
+wget https://github.com/coolsnowwolf/lede/raw/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
+popd
+
+# Patch FireWall 增添fullcone功能
+mkdir package/network/config/firewall/patches
+wget -P package/network/config/firewall/patches/ https://github.com/project-openwrt/openwrt/raw/master/package/network/config/firewall/patches/fullconenat.patch
+
+# Patch LuCI 增添fullcone开关
+patch -p1 < ./luci-app-firewall_add_fullcone.patch
+
+# FullCone 相关组件
+svn co https://github.com/Lienol/openwrt/trunk/package/network/fullconenat ./package/network/fullconenat
+
+
+## SFE
+# Patch Kernel 支援SFE
+pushd target/linux/generic/hack-5.4
+wget https://github.com/coolsnowwolf/lede/raw/master/target/linux/generic/hack-5.4/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+popd
+
+# Patch LuCI 增添SFE开关
+patch -p1 < ./luci-app-firewall_add_sfe_switch.patch
+
+# SFE 相关组件
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe package/lean/shortcut-fe
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/fast-classifier package/lean/fast-classifier
+mv ../etc/init.d/shortcut-fe package/base-files/files/etc/init.d/
+
+# luci-app-sfe
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-sfe ./package/lean/luci-app-sfe
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt ./package/lean/pdnsd-alt
